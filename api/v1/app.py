9c8e2fcf5b7f505f9f3abb205afe4f246e8edb42
Nyenzo <nyenzoisabwa@gmail.com>
2024-01-09 14:10:09 +0300

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""module for the api endpoint"""
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
-from os import getenv
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def handle_teardown(exception):
-    """Removes current session in sqlalchemy"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def hadle_404_error(ex):
-    """handles the 404 error"""
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    
